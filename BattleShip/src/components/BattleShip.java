70a58e96f8ba5426fb20755d6e0f4b7f7766045b
Hanjunhee-1 <hanjuni857@naver.com>
2023-06-08 18:24:59 +0900

    fix: 각각의 배를 컴포넌트화 시켜주기 위해 수정

--- BattleShip/src/components/BattleShip.java
+++ BattleShip/src/components/BattleShip.java
@@ -1,12 +1,23 @@
 package components;
 
-public class BattleShip {
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class BattleShip extends JPanel{
     private int point;
     private String name;
+    private JLabel nameLabel;
 
     public BattleShip(int point, String name) {
         this.point = point;
         this.name = name;
+
+        nameLabel = new JLabel(name);
+        nameLabel.setFont(new Font("Ink Free", Font.BOLD, 7));
+
+        this.add(nameLabel);
     }
 
     public int getPoint() {
