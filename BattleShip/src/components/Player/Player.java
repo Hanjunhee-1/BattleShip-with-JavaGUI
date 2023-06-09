e76e7695837f157032ae97583315b7d5f61e66af
Hanjunhee-1 <hanjuni857@naver.com>
2023-06-09 18:50:16 +0900

    feat: Player 이름과 점수를 표시하기 위한 컴포넌트 생성

---
+++ BattleShip/src/components/Player/Player.java
@@ -0,0 +1,61 @@
+package components.Player;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class Player extends JPanel{
+    private String name;
+    private int score;
+    private JLabel nameLabel;
+    private JPanel namePanel;
+    private JLabel scoreLabel;
+    private JPanel scorePanel;
+
+    public Player(String name, Color c) {
+        this.name = name;
+        this.score = 0;
+
+        nameLabel = new JLabel(name);
+        namePanel = new JPanel();
+        namePanel.add(nameLabel);
+
+        scoreLabel = new JLabel(Integer.toString(score));
+        scorePanel = new JPanel();
+        scorePanel.add(scoreLabel);
+
+        /* 확인용 나중에 지워야 됨 */
+        nameLabel.setOpaque(true);
+        nameLabel.setBackground(Color.red);
+
+        /* 확인용 나중에 지워야 됨 */
+        scoreLabel.setOpaque(true);
+        scoreLabel.setBackground(Color.blue);
+
+        this.setLayout(new BorderLayout());
+        this.setSize(120, 90);
+        this.setBorder(new LineBorder(c, 2));
+
+        this.add(namePanel, BorderLayout.NORTH);
+        this.add(scorePanel, BorderLayout.SOUTH);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setScoreLabel() {
+        scoreLabel.setText(Integer.toString(getScore()));
+    }
+}
