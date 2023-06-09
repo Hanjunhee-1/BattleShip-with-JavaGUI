05dc39b8bd1b3416aaed39376c3148c7b3fc0e55
Hanjunhee-1 <hanjuni857@naver.com>
2023-06-10 01:05:52 +0900

    fix: Button 의 모서리를 약간 둥글게 수정

--- BattleShip/src/common/Button.java
+++ BattleShip/src/common/Button.java
@@ -2,19 +2,43 @@ package common;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicButtonUI;
 
 public class Button extends JPanel {
     private JButton button;
 
     public Button() {
-        button = new JButton();
+        button = new JButton() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                if (getModel().isPressed()) {
+                    g.setColor(getBackground().darker());
+                } else if (getModel().isRollover()) {
+                    g.setColor(getBackground().brighter());
+                } else {
+                    g.setColor(getBackground());
+                }
+                g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 10, 10);
+                super.paintComponent(g);
+            }
+
+            @Override
+            protected void paintBorder(Graphics g) {
+                g.setColor(getForeground());
+                g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 10, 10);
+            }
+        };
+
         button.setPreferredSize(new Dimension(60, 20));
-        // button.setText("click!");
         button.setBorder(new LineBorder(Color.black, 1));
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setUI(new BasicButtonUI());
 
         this.add(button);
     }
